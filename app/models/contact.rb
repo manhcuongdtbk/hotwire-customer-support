11e5b57e7e11493762c21ab6171743af896a9e85
manhcuongdtbk <manhcuongdtbk@gmail.com>
2021-12-23 00:06:41 +0700

    Use turbo stream to prepend a newly created conversation to the conversations index page

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,4 @@
 class Contact < ApplicationRecord
+  has_many :posts, as: :author, dependent: :nullify
+  has_many :conversations, dependent: :nullify
 end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,3 +1,7 @@
 class Conversation < ApplicationRecord
+  has_many :posts, dependent: :destroy
+
   belongs_to :contact
+
+  broadcasts_to ->(_conversation) { 'conversations' }, inserts_by: :prepend, target: 'conversations'
 end
