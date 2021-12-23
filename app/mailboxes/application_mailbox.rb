8fc5361bafc7131257d779f2e62bcb3d15fe9fa5
manhcuongdtbk <manhcuongdtbk@gmail.com>
2021-12-24 00:07:42 +0700

    Convert email attachments to action text attachment

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -2,7 +2,23 @@ class ApplicationMailbox < ActionMailbox::Base
   routing /^conversation-(\d+)@/i => :reply
   routing all: :conversation
 
+  attr_reader :attachments
+
+  before_processing :save_attachments
+
   protected
+
+  def save_attachments
+    @attachments = mail.attachments.map do |attachment|
+      blob = ActiveStorage::Blob.create_and_upload!(
+        io: StringIO.new(attachment.body.to_s),
+        filename: attachment.filename,
+        content_type: attachment.content_type
+      )
+
+      { blob: blob, content_id: (attachment.content_id[1...-1] if attachment.content_id) }
+    end
+  end
 
   def from
     @from ||= mail[:from].address_list.addresses.first
@@ -19,10 +35,29 @@ class ApplicationMailbox < ActionMailbox::Base
     user.presence || contact
   end
 
+  # rubocop:disable Metrics/AbcSize
+  def process_html
+    document = Nokogiri::HTML(mail.html_part.body.decoded)
+
+    attachments.each do |attachment_hash|
+      content_id = attachment_hash[:content_id]
+      next unless content_id
+
+      blob = attachment_hash[:blob]
+      element = document.at_css "img[src='cid:#{content_id}']"
+      # rubocop:disable Layout/LineLength
+      element.replace "<action-text-attachment sgid=\"#{blob.attachable_sgid}\" content-type=\"#{blob.content_type}\" filename=\"#{blob.filename}\"></action-text-attachment>"
+      # rubocop:enable Layout/LineLength
+    end
+
+    document.at_css('body').inner_html.encode('utf-8')
+  end
+  # rubocop:enable Metrics/AbcSize
+
   def body
     if mail.multipart?
       if mail.html_part
-        mail.html_part.body.decoded
+        process_html
       elsif mail.text_part
         mail.text_part.body.decoded
       end
